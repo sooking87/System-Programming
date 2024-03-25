b8fbf707af7485d88a897ed9d52ec2098ccd0ee7
sooking87 <sksohn01@naver.com>
2024-03-25 20:35:06 +0900

    2024-03-25-laptop

---
+++ Week3/stquery.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "student.h"
+#define START_ID 1001001
+
+int main(int argc, char *argv[])
+{
+    struct student rec;
+    char c;
+    int id;
+    FILE *fp;
+    if (argc != 2) {
+        fprintf(stderr, "사용법: %s 파일이름\n", argv[0]);
+        exit(1);
+    }
+    if ((fp=fopen(argv[1], "rb")) == NULL) {
+        fprint(stderr, "파일 열기 오류\n");
+        exit(2);
+    }
+    do {
+        printf("검색할 학생의 학번 입력: ");
+        if (scanf("%d", &id) == 1) {
+            fseek(fp, (id - START_ID) *sizeof(rec), SEEK_SET);
+            if ((fread(&rec, sizeof(rec), 1, fp) > 0) && (rec.id != 0)) {
+                printf("학번: %8d 이름: %4s\n", rec.id, rec.name, rec.score);
+            }
+            else printf("레코드 %d 없음\n", id);
+
+            printf("계속하겠습니까?(Y/N)");
+            scanf(" %c", &c);
+        } 
+    } while (c == 'Y');
+    fclose(fp);
+    exit(0);
+    
+}
